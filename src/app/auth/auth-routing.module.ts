295ff855e0aebad87c54a930e837abc43b90f97a
Adrian Juszczak <adrian@juszczak.io>
2020-06-06 09:56:17 +0200

    Register routing
    Signed-off-by: Adrian Juszczak <adrian@juszczak.io>

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -2,8 +2,34 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { AuthComponent } from './auth.component';
+import { RegisterComponent } from './register/register.component';
 
-const routes: Routes = [{ path: '', component: AuthComponent }];
+const routes: Routes = [
+  {
+    path: '',
+    component: AuthComponent,
+    children: [
+      {
+        // /auth/register
+        path: 'register',
+        // 1.
+        /* loadChildren: () => import('./register/register.module') */
+        /*   .then(m => m.RegisterModule), */
+
+        // 2.
+        /* loadChildren: () => import('./register/register.module') */
+        /*   .then((importedModule) => importedModule.RegisterModule) */
+
+        // 3.
+        loadChildren: () => {
+          const importedModulePromise = import('./register/register.module');
+          return importedModulePromise
+            .then((importedModule) => importedModule.RegisterModule);
+        }
+      }
+    ]
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
